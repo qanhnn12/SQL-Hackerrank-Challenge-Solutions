77eea63590a91040136d12b9304f233348e29aa5
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-10-08 07:41:44 +0700

    Delete Advanced Selecr directory

--- Advanced Selecr/Type-of-Triangle.sql
+++
@@ -1,8 +0,0 @@
---MySQL & MS SQL Sever
-SELECT 
-    CASE WHEN A+B>C THEN
-        CASE WHEN A=B AND B=C THEN 'Equilateral'
-             WHEN A=B OR A=C OR B=C THEN 'Isosceles' 
-        ELSE 'Scalene' END
-    ELSE 'Not A Triangle' END
-FROM TRIANGLES;
