6ccd94532f6992359f5c7fb2533dff3913e3a12f
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-10-08 08:24:15 +0700

    Create Occupations.sql

---
+++ Advanced Select/Occupations.sql
@@ -0,0 +1,25 @@
+--MySQL & MS SQL Server
+SELECT 
+    MAX(CASE WHEN Occupation = 'Doctor' THEN Name END) AS Doctor,
+    MAX(CASE WHEN Occupation = 'Professor' THEN Name END) AS Professor,
+    MAX(CASE WHEN Occupation = 'Singer' THEN Name END) AS Singer,
+    MAX(CASE WHEN Occupation = 'Actor' THEN Name END) AS Actor
+FROM (
+    SELECT 
+        Name, 
+        Occupation,
+        ROW_NUMBER() OVER(PARTITION BY Occupation ORDER BY Name) AS rn
+    FROM OCCUPATIONS
+    ) tmp
+GROUP BY rn;
+
+--MS SQL Server
+SELECT 
+  [Doctor], [Professor], [Singer], [Actor]
+FROM (
+  SELECT 
+    Name, Occupation,
+    ROW_NUMBER() OVER (PARTITION BY Occupation ORDER BY Name) [RowNumber]
+  FROM Occupations ) AS tmp
+PIVOT (
+  MAX(Name) FOR Occupation IN ([Doctor], [Professor], [Singer], [Actor])) AS pvt;
