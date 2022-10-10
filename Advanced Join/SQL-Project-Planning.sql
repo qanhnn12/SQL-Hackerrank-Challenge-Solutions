a32a83bbdd031bdf41ab153953d6d6b842eb09c7
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-10-10 15:00:35 +0700

    Update SQL-Project-Planning.sql

--- Advanced Join/SQL-Project-Planning.sql
+++ Advanced Join/SQL-Project-Planning.sql
@@ -12,16 +12,16 @@ ORDER BY DATEDIFF(End_Date, Start_Date), Start_Date;
 ---Method 2: 
 SELECT MIN(start_date), MAX(end_date)
 FROM (
-	SELECT 
-		start_date, end_date, 
-		SUM(flag) OVER(ORDER BY start_date) AS grp
-	FROM (
-		SELECT 
-			task_id, start_date, end_date,
-			CASE WHEN LAG(end_date) OVER(ORDER BY start_date) = start_date 
-				 THEN 0 ELSE 1 END AS flag
-		FROM Projects
-		) t1
+    SELECT 
+        start_date, end_date, 
+        SUM(flag) OVER(ORDER BY start_date) AS grp
+    FROM (
+        SELECT 
+            task_id, start_date, end_date,
+            CASE WHEN LAG(end_date) OVER(ORDER BY start_date) = start_date 
+                 THEN 0 ELSE 1 END AS flag
+        FROM Projects
+        ) t1
         ) t2
 GROUP BY grp
 ORDER BY DATEDIFF(MAX(end_date), MIN(start_date)), MIN(start_date)
