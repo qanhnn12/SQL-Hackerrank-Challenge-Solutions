581ec7efe0559bc82429f6daa39cbd17d0884e14
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-10-10 14:36:33 +0700

    Create SQL-Project-Planning.sql

---
+++ Advanced Join/SQL-Project-Planning.sql
@@ -0,0 +1,17 @@
+--MySQL
+SELECT Start_Date, End_Date
+FROM 
+    (SELECT Start_Date FROM Projects WHERE Start_Date NOT IN (SELECT End_Date FROM Projects)) a,
+    (SELECT End_Date FROM Projects WHERE End_Date NOT IN (SELECT Start_Date FROM Projects)) b 
+WHERE Start_Date < End_Date
+GROUP BY Start_Date 
+ORDER BY DATEDIFF(End_Date, Start_Date), Start_Date
+
+--MS SQL Server
+SELECT Start_Date, End_Date
+FROM 
+    (SELECT Start_Date FROM Projects WHERE Start_Date NOT IN (SELECT End_Date FROM Projects)) a,
+    (SELECT End_Date FROM Projects WHERE End_Date NOT IN (SELECT Start_Date FROM Projects)) b 
+WHERE Start_Date < End_Date
+GROUP BY Start_Date 
+ORDER BY DATEDIFF(Day, Start_Date, End_Date), Start_Date
