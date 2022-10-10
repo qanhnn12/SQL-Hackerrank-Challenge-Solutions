ccf887ea00cdc6f59808be4a7b9cb149f5910fdb
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-10-10 15:00:06 +0700

    Update SQL-Project-Planning.sql

--- Advanced Join/SQL-Project-Planning.sql
+++ Advanced Join/SQL-Project-Planning.sql
@@ -1,4 +1,5 @@
 --MySQL
+---Method 1: 
 SET sql_mode = '';
 SELECT Start_Date, End_Date
 FROM 
@@ -6,7 +7,25 @@ FROM
     (SELECT End_Date FROM Projects WHERE End_Date NOT IN (SELECT Start_Date FROM Projects)) b 
 WHERE Start_Date < End_Date
 GROUP BY Start_Date 
-ORDER BY DATEDIFF(End_Date, Start_Date), Start_Date
+ORDER BY DATEDIFF(End_Date, Start_Date), Start_Date;
+
+---Method 2: 
+SELECT MIN(start_date), MAX(end_date)
+FROM (
+	SELECT 
+		start_date, end_date, 
+		SUM(flag) OVER(ORDER BY start_date) AS grp
+	FROM (
+		SELECT 
+			task_id, start_date, end_date,
+			CASE WHEN LAG(end_date) OVER(ORDER BY start_date) = start_date 
+				 THEN 0 ELSE 1 END AS flag
+		FROM Projects
+		) t1
+        ) t2
+GROUP BY grp
+ORDER BY DATEDIFF(MAX(end_date), MIN(start_date)), MIN(start_date)
+
 
 --MS SQL Server
 SELECT MIN(start_date), MAX(end_date)
@@ -23,4 +42,4 @@ FROM (
         ) t1
         ) t2
 GROUP BY grp
-ORDER BY DATEDIFF(DAY, MIN(start_date), MAX(end_date)), MIN(start_date)
+ORDER BY DATEDIFF(DAY, MIN(start_date), MAX(end_date)), MIN(start_date);
