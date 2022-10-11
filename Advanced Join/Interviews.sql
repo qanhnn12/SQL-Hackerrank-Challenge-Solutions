258e70d934ad697870aac4077857ed29efc51bb6
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-10-11 10:42:40 +0700

    Create Interviews.sql

---
+++ Advanced Join/Interviews.sql
@@ -0,0 +1,25 @@
+--MySQL & MS SQL Server
+
+SELECT submission_date,
+
+      (SELECT COUNT(DISTINCT hacker_id)
+       FROM Submissions AS s2
+       WHERE s2.submission_date = s1.submission_date
+       AND (SELECT COUNT(DISTINCT submission_date)
+            FROM Submissions AS s3
+            WHERE s3.hacker_id = s2.hacker_id 
+            AND s3.submission_date < s2.submission_date
+            ) = DATEDIFF(s1.submission_date,'2016-03-01')),
+                                  
+      (SELECT hacker_id
+       FROM Submissions AS s4
+       WHERE s4.submission_date = s1.submission_date
+       GROUP BY hacker_id
+       ORDER BY COUNT(submission_id) DESC, hacker_id 
+       LIMIT 1) AS h_id,
+        
+      (SELECT name
+       FROM Hackers
+       WHERE hacker_id = h_id)
+        
+FROM (SELECT DISTINCT submission_date FROM Submissions) AS s1;
