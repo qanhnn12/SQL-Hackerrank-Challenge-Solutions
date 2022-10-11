80eb9892712bc4696596c876834c371296cdd59a
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-10-11 19:41:03 +0700

    Update Type-of-Triangle.sql

--- Advanced Select/Type-of-Triangle.sql
+++ Advanced Select/Type-of-Triangle.sql
@@ -1,6 +1,6 @@
 --MySQL & MS SQL Sever
 SELECT 
-    CASE WHEN A+B> C THEN
+    CASE WHEN A+B>C THEN
         CASE WHEN A=B AND B=C THEN 'Equilateral'
              WHEN A=B OR A=C OR B=C THEN 'Isosceles' 
         ELSE 'Scalene' END
