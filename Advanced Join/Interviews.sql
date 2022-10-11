2dc1e3f8ad037b5374c69e6a45840e52d954f55f
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-10-11 14:24:35 +0700

    Create Interviews.sql

---
+++ Advanced Join/Interviews.sql
@@ -0,0 +1,30 @@
+-- MySQL & MS SQL Server
+WITH VStats AS (
+SELECT challenge_id, 
+	SUM(total_views) AS total_views, 
+	SUM(total_unique_views) AS total_unique_views
+FROM View_Stats 
+GROUP BY challenge_id)
+,
+SStats AS (
+SELECT challenge_id, 
+	SUM(total_submissions) AS total_submissions, 
+	SUM(total_accepted_submissions) AS total_accepted_submissions
+FROM Submission_Stats 
+GROUP BY challenge_id)
+
+SELECT 
+    ct.contest_id, ct.hacker_id, ct.name, 
+    SUM(total_submissions), SUM(total_accepted_submissions),
+    SUM(total_views), SUM(total_unique_views)
+FROM Contests ct
+JOIN Colleges cl ON ct.contest_id = cl.contest_id
+JOIN Challenges chl ON cl.college_id = chl.college_id
+LEFT JOIN VStats vs ON chl.challenge_id = vs.challenge_id
+LEFT JOIN SStats ss ON chl.challenge_id = ss.challenge_id
+GROUP BY ct.contest_id, ct.hacker_id, ct.name
+HAVING SUM(total_submissions) != 0 
+OR SUM(total_accepted_submissions) != 0 
+OR SUM(total_views) != 0 
+OR SUM(total_unique_views) != 0
+ORDER BY ct.contest_id;
