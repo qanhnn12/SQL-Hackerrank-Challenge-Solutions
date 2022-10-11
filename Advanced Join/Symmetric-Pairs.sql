1bb49595963b5d37838b8ab7d2049aa9d4617824
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-10-11 08:54:52 +0700

    Update Symmetric-Pairs.sql

--- Advanced Join/Symmetric-Pairs.sql
+++ Advanced Join/Symmetric-Pairs.sql
@@ -4,15 +4,14 @@ SELECT f1.X, f1.Y
 FROM Functions f1, Functions f2
 WHERE f1.X = f2.Y
 AND f2.X = f1.Y
-AND f1.X <= f1.Y
 GROUP BY f1.X, f1.Y
-HAVING COUNT(f1.X)>1 or f1.X<f1.Y
-ORDER BY f1.X 
+HAVING COUNT(f1.X)>1 OR f1.X<f1.Y
+ORDER BY f1.X;
 
 # Method 2:
 SELECT f1.X, f1.Y 
 FROM Functions f1
 JOIN Functions f2 ON f1.X=f2.Y AND f1.Y=f2.X
 GROUP BY f1.X, f1.Y
-HAVING COUNT(f1.X)>1 or f1.X<f1.Y
-ORDER BY f1.X 
+HAVING COUNT(f1.X)>1 OR f1.X<f1.Y
+ORDER BY f1.X;
