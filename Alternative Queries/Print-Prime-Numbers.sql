c5467dbdb4bb8b2373aaf5c90215ecb7741068ee
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-10-11 15:25:29 +0700

    Update Print-Prime-Numbers.sql

--- Alternative Queries/Print-Prime-Numbers.sql
+++ Alternative Queries/Print-Prime-Numbers.sql
@@ -1,18 +1,35 @@
 --MySQL
-SET @potential_prime = 1;
+---Method 1:
+SET @prime = 1;
 SET @divisor = 1;
 
-SELECT GROUP_CONCAT(POTENTIAL_PRIME SEPARATOR '&') 
+SELECT GROUP_CONCAT(a SEPARATOR '&') 
 FROM
-    (SELECT @potential_prime := @potential_prime + 1 AS POTENTIAL_PRIME       
-    FROM information_schema.tables t1, information_schema.tables t2             # Since each information_schema.table has less than 1000 rows, use 2 tables
+    (SELECT @prime := @prime + 1 AS a       
+    FROM information_schema.tables t1, information_schema.tables t2             
     LIMIT 1000) list_of_potential_primes
 WHERE NOT EXISTS (
     SELECT * FROM
-        (SELECT @divisor := @divisor + 1 AS DIVISOR 
+        (SELECT @divisor := @divisor + 1 AS b 
         FROM information_schema.tables t3, information_schema.tables t4
         LIMIT 1000) list_of_divisors
-    WHERE MOD(POTENTIAL_PRIME, DIVISOR) = 0 AND POTENTIAL_PRIME != DIVISOR);
+    WHERE MOD(a, b) = 0 AND a != b);
+
+---Method 2:
+WITH RECURSIVE cte(num) AS (
+  SELECT 2
+  UNION ALL 
+  SELECT num+1 FROM cte
+  WHERE num+1 < 1000
+  )
+  
+SELECT GROUP_CONCAT(a.num SEPARATOR '&')        # a.num = prime
+FROM cte a
+WHERE NOT EXISTS (
+   SELECT * 
+   FROM cte b
+   WHERE MOD(a.num, b.num)=0                    # b.num = divisor
+   AND a.num != b.num )
     
     
 --MS SQL Server
@@ -23,7 +40,7 @@ WITH cte(num) AS (
   WHERE num+1 < 1000
   )
   
-SELECT STRING_AGG(a.num, '&')       # a.num = potential_prime
+SELECT STRING_AGG(a.num, '&')       # a.num = prime
 FROM cte a
 WHERE NOT EXISTS (
    SELECT * 
